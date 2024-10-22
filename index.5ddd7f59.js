36565a51608f9ef5018e6456f1b491235ac010ec
Anna <annanicky90gmail.com>
2024-10-22 13:14:57 +0400

    make build

---
+++ index.5ddd7f59.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".wall"),e=document.querySelector(".spider"),l=t.clientWidth,c=t.clientHeight;e.style.top="".concat(c/2,"px"),e.style.left="".concat(l/2,"px"),e.style.transform="translate(-50%, -50%)";
+//# sourceMappingURL=index.5ddd7f59.js.map
